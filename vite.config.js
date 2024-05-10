add4f565a11ed2a8f76e3e17d12e02ee0f3602d1
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-05-10 16:56:57 +0200

    fix: we prayed for times like this

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	kit: {
-		adapter: adapter()
-	}
 });
