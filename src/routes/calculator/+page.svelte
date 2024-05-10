6d438599795e08496c5173bab6ba6e13235da946
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-05-10 04:03:45 +0200

    feat: sveltekit deez

---
+++ src/routes/calculator/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <title>Faye Keller · Graal Sellables Calculator</title>
+    <meta name="description" content="A calculator for Graal Era sellables" />
+</svelte:head>
+
+<main>
+    <h1>Faye Keller</h1>
+</main>
