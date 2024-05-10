ad1faae4520b6d41dd6288c1b5d0a6a1403cecf7
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-05-10 16:23:26 +0200

    fix: not deploying to vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-static'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	kit: { adapter: adapter() },
 });
