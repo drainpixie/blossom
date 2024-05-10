d14584290baa07adfa5c1fa7c6c5ed0246dbfa9a
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-05-10 16:37:02 +0200

    fix: try svelet adapter

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	kit: {
+		adapter: adapter()
+	}
 });
