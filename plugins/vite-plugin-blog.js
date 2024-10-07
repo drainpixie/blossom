7455fe580e04da5c7b8fb2b82f72a8b3d1884f83
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-10-07 14:59:42 +0200

    fix(plugin-blog): 🐛 more hacky asset resolution

--- plugins/vite-plugin-blog.js
+++ plugins/vite-plugin-blog.js
@@ -37,7 +37,7 @@ const createHTML = ({ title, date, tags, content, description }) => `
 <html>
     <head>
         <title>${title}</title>
-        <link rel="stylesheet" href="/assets/css/post.css" />
+        <link rel="stylesheet" href="../assets/css/post.css" />
         
         <meta charset="UTF-8">
         <meta name="date" content="${date}" />
@@ -54,23 +54,23 @@ const createHTML = ({ title, date, tags, content, description }) => `
         <meta property="twitter:title" content="Faye Keller">
         <meta property="twitter:description" content="${description}">
         
-        <link rel="apple-touch-icon" sizes="57x57" href="/assets/icons/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="/assets/icons/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="/assets/icons/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="/assets/icons/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="/assets/icons/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="/assets/icons/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="/assets/icons/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
-        <meta name="msapplication-TileImage" content="/assets/icons/ms-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="../assets/icons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="../assets/icons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="../assets/icons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="../assets/icons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="../assets/icons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="../assets/icons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="../assets/icons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="../assets/icons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="../assets/icons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="../assets/icons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="../assets/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="../assets/icons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="../assets/icons/favicon-16x16.png">
+        <meta name="msapplication-TileImage" content="../assets/icons/ms-icon-144x144.png">
         <meta name="msapplication-TileColor" content="#e9eb93">
         <meta name="theme-color" content="#e9eb93">    
-        <link rel="manifest" href="/assets/icons/manifest.json">
+        <link rel="manifest" href="../assets/icons/manifest.json">
     </head>
     <body>
         <div class="canvas-container">
@@ -79,7 +79,7 @@ const createHTML = ({ title, date, tags, content, description }) => `
         <h1><a class="back" href="/">${title}</a></h1>
         ${content}
     </body>
-    <script type="module" src="/assets/js/index.js"></script>
+    <script type="module" src="../assets/js/index.js"></script>
 </html>`;
 
 const createPostPreview = ({ date, tags, link, title }) => `
@@ -212,7 +212,7 @@ export function blogPlugin() {
               ),
             )
               .filter((x) => x[0].startsWith("assets"))
-              .map((x) => ({ lookup: `/${x[0]}`, replace: `/${x[1].file}` }))
+              .map((x) => ({ lookup:`../${x[0]}`, replace: `../${x[1].file}` }))
               .reduce(
                 (content, x) => content.replace(x.lookup, x.replace),
                 await readFile(
