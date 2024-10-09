4702158bfbdd2a8b6d86665fb6e1659f00e69e15
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-10-10 00:11:04 +0200

    fix(js/index): 🐛 only run fetch on /
    ù

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,9 @@
 import { initPrism } from "./three";
 
-document.addEventListener('DOMContentLoaded', initPrism);
+document.addEventListener("DOMContentLoaded", initPrism);
 
-if (import.meta.env.MODE === 'development') {
-    const { initPosts } = await import('./fetch.js');
-    initPosts()
+if (import.meta.env.MODE === "development" && location.pathname === "/") {
+  const { initPosts } = await import("./fetch.js");
+  initPosts();
 }
+
