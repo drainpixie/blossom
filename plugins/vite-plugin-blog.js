cabd839bd52da9c5f91de73ff803f9be3288492d
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-10-20 14:11:24 +0200

    feat: ✨ meta tag + minor mod

--- plugins/vite-plugin-blog.js
+++ plugins/vite-plugin-blog.js
@@ -56,6 +56,9 @@ const createHTML = ({ title, date, tags, content, description }) => `
         <title>${title}</title>
         <link rel="stylesheet" href="/assets/css/post.css" />
         
+				<meta
+					http-equiv="Content-Security-Policy"
+					content="default-src 'self'; img-src https://*; child-src 'none';" />
         <meta charset="UTF-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="date" content="${date}" />
